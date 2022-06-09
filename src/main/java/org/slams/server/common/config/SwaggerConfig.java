e449ea2dd6aa00800863a5a4059cd3c2af1b9f9d
coloveding <coloveding@gmail.com>
2022-06-09 17:30:44 +0900

    feat: swagger authorization button 추가

--- src/main/java/org/slams/server/common/config/SwaggerConfig.java
+++ src/main/java/org/slams/server/common/config/SwaggerConfig.java
@@ -6,9 +6,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -16,22 +22,41 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket docket() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.useDefaultResponseMessages(false)
-			.apiInfo(apiInfo())
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.any())
-			.build();
+		return new Docket(DocumentationType.OAS_30)
+				.securityContexts(List.of(securityContext()))
+				.securitySchemes(List.of(apikey()))
+				.useDefaultResponseMessages(false)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
-			.title("Slam Web Application Api")
-			.description("길거리 농구 매칭 앱")
-			.version("1.0.0")
-			.license("Copyright Slam")
-			.build();
+				.title("Slam Web Application Api")
+				.description("길거리 농구 매칭 앱")
+				.version("1.0.0")
+				.license("Copyright Slam")
+				.build();
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return List.of(new SecurityReference("Authorization", authorizationScopes));
+	}
+
+	private ApiKey apikey(){
+		return new ApiKey("Authorization", "Authorization", "header");
 	}
 
 }
