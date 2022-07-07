b8fdc69e5bbac12aed0843c1bd70c33a7be2b036
yekyeong <kwonliz6.6@gmail.com>
2022-07-07 22:45:38 +0900

    refactor : follow 도메인 api response 전부 id의 type String으로 변경

--- src/main/java/org/slams/server/follow/dto/response/FollowRecieverDto.java
+++ src/main/java/org/slams/server/follow/dto/response/FollowRecieverDto.java
@@ -9,18 +9,18 @@ import org.slams.server.user.entity.User;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FollowRecieverDto {
 
-	private Long id;
+	private String id;
 	private String nickname;
 	private String profileImage;
 
-	private FollowRecieverDto(Long id, String nickname, String profileImage) {
+	private FollowRecieverDto(String id, String nickname, String profileImage) {
 		this.id = id;
 		this.nickname = nickname;
 		this.profileImage = profileImage;
 	}
 
 	public static FollowRecieverDto toDto(User reciever) {
-		return new FollowRecieverDto(reciever.getId(), reciever.getNickname(), reciever.getProfileImage());
+		return new FollowRecieverDto(reciever.getId().toString(), reciever.getNickname(), reciever.getProfileImage());
 	}
 
 }
