cbee43935751aa60df94eef4ba625c85af511dd5
KwonYeKyeong <65434196+KwonYeKyeong@users.noreply.github.com>
2022-09-26 18:18:40 +0900

    refactor/new court 66 - api response 구조 변경 (#67)
    * refactor : api response 구조 수정
    
    * chore : 불필요한 파일 제거

--- src/main/java/org/slams/server/court/dto/response/CourtDetailResponse.java
+++ src/main/java/org/slams/server/court/dto/response/CourtDetailResponse.java
@@ -5,19 +5,37 @@ import org.slams.server.common.api.BaseResponse;
 import org.slams.server.court.entity.Court;
 import org.slams.server.court.entity.Texture;
 
-@Getter
-public class CourtDetailResponse {
+import java.time.LocalDateTime;
 
-	private CourtDetailDto court;
+@Getter
+public class CourtDetailResponse extends BaseResponse {
+
+	private String id;
+	private String name;
+	private double latitude;
+	private double longitude;
+	private String image;
+	private int basketCount;
+	private Texture texture;
 	private Long reservationMaxCount;
 
-	private CourtDetailResponse(CourtDetailDto court, Long reservationMaxCount) {
-		this.court = court;
+	public CourtDetailResponse(String id, String name, double latitude, double longitude,
+							   String image, int basketCount, Texture texture,
+							   LocalDateTime createdAt, LocalDateTime updatedAt, Long reservationMaxCount) {
+		super(createdAt, updatedAt);
+		this.id = id;
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.image = image;
+		this.basketCount = basketCount;
+		this.texture = texture;
 		this.reservationMaxCount = reservationMaxCount;
 	}
 
 	public static CourtDetailResponse toResponse(Court court, Long reservationMaxCount) {
-		return new CourtDetailResponse(CourtDetailDto.toDto(court), reservationMaxCount);
+		return new CourtDetailResponse(court.getId().toString(), court.getName(), court.getLatitude(), court.getLongitude(),
+			court.getImage(), court.getBasketCount(), court.getTexture(), court.getCreatedAt(), court.getUpdatedAt(), reservationMaxCount);
 	}
 
 }
--- src/main/java/org/slams/server/court/dto/response/NewCourtInReadyLookUpResponse.java
+++ src/main/java/org/slams/server/court/dto/response/NewCourtInReadyLookUpResponse.java
@@ -4,22 +4,48 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.slams.server.court.entity.NewCourt;
+import org.slams.server.court.entity.Status;
+import org.slams.server.court.entity.Texture;
 import org.slams.server.user.dto.response.CreatorDto;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NewCourtInReadyLookUpResponse {
 
-	private NewCourtDto newCourt;
+	private String id;
+	private String name;
+	private double latitude;
+	private double longitude;
+	private String image;
+	private Texture texture;
+	private int basketCount;
+	private Status status;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 	private CreatorDto creator;
 
-	private NewCourtInReadyLookUpResponse(NewCourtDto newCourt, CreatorDto creator) {
-		this.newCourt = newCourt;
+	private NewCourtInReadyLookUpResponse(String id, String name, double latitude, double longitude,
+										  String image, Texture texture, int basketCount, Status status,
+										  LocalDateTime createdAt, LocalDateTime updatedAt, CreatorDto creator) {
+		this.id = id;
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.image = image;
+		this.texture = texture;
+		this.basketCount = basketCount;
+		this.status = status;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
 		this.creator = creator;
 	}
 
 	public static NewCourtInReadyLookUpResponse toResponse(NewCourt newCourt) {
-		return new NewCourtInReadyLookUpResponse(NewCourtDto.toDto(newCourt), CreatorDto.toDto(newCourt.getProposer()));
+		return new NewCourtInReadyLookUpResponse(newCourt.getId().toString(), newCourt.getName(), newCourt.getLatitude(), newCourt.getLongitude(),
+			newCourt.getImage(), newCourt.getTexture(), newCourt.getBasketCount(), newCourt.getStatus(),
+			newCourt.getCreatedAt(), newCourt.getUpdatedAt(), CreatorDto.toDto(newCourt.getProposer()));
 	}
 
 }
