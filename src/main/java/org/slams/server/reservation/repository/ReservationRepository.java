38b10ba9fa8ee8001d76995d2614dd207db5adb9
yekyeong <kwonliz6.6@gmail.com>
2022-11-15 01:23:39 +0900

    chore : 코드 재정렬

--- src/main/java/org/slams/server/reservation/repository/ReservationRepository.java
+++ src/main/java/org/slams/server/reservation/repository/ReservationRepository.java
@@ -26,11 +26,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("endLocalDateTime") LocalDateTime endLocalDateTime
            );
 
-    @Query("SELECT r FROM Reservation r JOIN FETCH r.court WHERE r.user.id=:userId AND r.startTime>:localDateTime")
-    List<Reservation> findByUserFromStartTime(
-        @Param("userId") Long userId,
-        @Param("localDateTime") LocalDateTime localDateTime
-    );
+	@Query("SELECT r FROM Reservation r JOIN FETCH r.court WHERE r.user.id=:userId AND r.startTime>:localDateTime")
+	List<Reservation> findByUserFromStartTime(
+		@Param("userId") Long userId,
+		@Param("localDateTime") LocalDateTime localDateTime
+	);
 
     @Query("SELECT r.user.id FROM Reservation r WHERE r.court.id=:courtId")
     List<Long> findBeakerIdByCourtId(
@@ -66,11 +66,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     Optional<Reservation> findByCourtAndUser(Court court, User user);
 
-    @Query("SELECT r FROM Reservation r JOIN FETCH r.user WHERE r.court.id = :courtId AND r.startTime < :endTime AND r.endTime > :startTime ORDER BY r.startTime")
-    List<Reservation> findByCourtIdAndDateBetweenOrderByStartTime(
-        @Param("courtId") Long courtId,
-        @Param("startTime") LocalDateTime startTime,
-        @Param("endTime") LocalDateTime endTime
-    );
+	@Query("SELECT r FROM Reservation r JOIN FETCH r.user WHERE r.court.id = :courtId AND r.startTime < :endTime AND r.endTime > :startTime ORDER BY r.startTime")
+	List<Reservation> findByCourtIdAndDateBetweenOrderByStartTime(
+		@Param("courtId") Long courtId,
+		@Param("startTime") LocalDateTime startTime,
+		@Param("endTime") LocalDateTime endTime
+	);
 
 }
