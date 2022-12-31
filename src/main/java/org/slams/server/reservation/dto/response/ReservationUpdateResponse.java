03b0fbe74a88960405c2cec9879d4f6c5d6b9592
yekyeong <kwonliz6.6@gmail.com>
2022-12-31 23:35:02 +0900

    refactor : response 속 예약 관련 시간 LocalDateTime에서 Instant로 바꾸기

--- src/main/java/org/slams/server/reservation/dto/response/ReservationUpdateResponse.java
+++ src/main/java/org/slams/server/reservation/dto/response/ReservationUpdateResponse.java
@@ -4,17 +4,19 @@ import lombok.Getter;
 import org.slams.server.common.api.BaseResponse;
 import org.slams.server.reservation.entity.Reservation;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Getter
 public class ReservationUpdateResponse extends BaseResponse {
 
 	private String id;
-	private LocalDateTime startTime;
-	private LocalDateTime endTime;
+	private Instant startTime;
+	private Instant endTime;
 	private Boolean hasBall;
 
-	private ReservationUpdateResponse(LocalDateTime createdAt, LocalDateTime updatedAt, String id, LocalDateTime startTime, LocalDateTime endTime, Boolean hasBall) {
+	private ReservationUpdateResponse(LocalDateTime createdAt, LocalDateTime updatedAt, String id, Instant startTime, Instant endTime, Boolean hasBall) {
 		super(createdAt, updatedAt);
 		this.id = id;
 		this.startTime = startTime;
@@ -24,7 +26,7 @@ public class ReservationUpdateResponse extends BaseResponse {
 
 	public static ReservationUpdateResponse of(Reservation reservation) {
 		return new ReservationUpdateResponse(reservation.getCreatedAt(), reservation.getUpdatedAt(),
-			reservation.getId().toString(), reservation.getStartTime(), reservation.getEndTime(), reservation.isHasBall());
+			reservation.getId().toString(), reservation.getStartTime().toInstant(ZoneOffset.UTC), reservation.getEndTime().toInstant(ZoneOffset.UTC), reservation.isHasBall());
 	}
 
 }
