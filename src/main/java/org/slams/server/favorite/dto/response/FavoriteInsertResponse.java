4690ac6eaccf5e09d5e2da0eece2e5db09cedf31
yekyeong <kwonliz6.6@gmail.com>
2023-02-12 23:13:56 +0900

    refactor : favorite 관련 dto 시간 관련 필드값 utc로 통일
    createdAt, updatedAt

--- src/main/java/org/slams/server/favorite/dto/response/FavoriteInsertResponse.java
+++ src/main/java/org/slams/server/favorite/dto/response/FavoriteInsertResponse.java
@@ -5,7 +5,8 @@ import org.slams.server.common.api.BaseResponse;
 import org.slams.server.court.dto.response.CourtInMapDto;
 import org.slams.server.favorite.entity.Favorite;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.ZoneOffset;
 
 @Getter
 public class FavoriteInsertResponse extends BaseResponse {
@@ -13,15 +14,15 @@ public class FavoriteInsertResponse extends BaseResponse {
 	private String id;
 	private CourtInMapDto court;
 
-	private FavoriteInsertResponse(LocalDateTime createdAt, LocalDateTime updatedAt, String id, CourtInMapDto court) {
+	private FavoriteInsertResponse(Instant createdAt, Instant updatedAt, String id, CourtInMapDto court) {
 		super(createdAt, updatedAt);
 		this.id = id;
 		this.court = court;
 	}
 
 	public static FavoriteInsertResponse toResponse(Favorite favorite) {
-		return new FavoriteInsertResponse(
-			favorite.getCreatedAt(), favorite.getUpdatedAt(), favorite.getId().toString(), CourtInMapDto.toDto(favorite.getCourt()));
+		return new FavoriteInsertResponse(favorite.getCreatedAt().toInstant(ZoneOffset.UTC), favorite.getUpdatedAt().toInstant(ZoneOffset.UTC),
+			favorite.getId().toString(), CourtInMapDto.toDto(favorite.getCourt()));
 	}
 
 }
--- src/main/java/org/slams/server/favorite/dto/response/FavoriteLookUpResponse.java
+++ src/main/java/org/slams/server/favorite/dto/response/FavoriteLookUpResponse.java
@@ -5,7 +5,9 @@ import org.slams.server.common.api.BaseResponse;
 import org.slams.server.court.dto.response.CourtInMapDto;
 import org.slams.server.favorite.entity.Favorite;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Getter
 public class FavoriteLookUpResponse extends BaseResponse {
@@ -13,15 +15,15 @@ public class FavoriteLookUpResponse extends BaseResponse {
 	private String id;
 	private CourtInMapDto court;
 
-	private FavoriteLookUpResponse(LocalDateTime createdAt, LocalDateTime updatedAt, String id, CourtInMapDto court) {
+	private FavoriteLookUpResponse(Instant createdAt, Instant updatedAt, String id, CourtInMapDto court) {
 		super(createdAt, updatedAt);
 		this.id = id;
 		this.court = court;
 	}
 
 	public static FavoriteLookUpResponse toResponse(Favorite favorite) {
-		return new FavoriteLookUpResponse(
-			favorite.getCreatedAt(), favorite.getUpdatedAt(), favorite.getId().toString(), CourtInMapDto.toDto(favorite.getCourt()));
+		return new FavoriteLookUpResponse(favorite.getCreatedAt().toInstant(ZoneOffset.UTC), favorite.getUpdatedAt().toInstant(ZoneOffset.UTC),
+			favorite.getId().toString(), CourtInMapDto.toDto(favorite.getCourt()));
 	}
 
 }
